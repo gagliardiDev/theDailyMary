663e3f693cb77095a75555b2e45b0f375178c6a9
Marcus Vinicius Galhardi Ferreira <magro.ferreira@gmail.com>
2025-04-15 23:31:28 +0000

    add  files

--- the-daily-mary/src/app/app.routes.ts
+++ the-daily-mary/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+// src/app/app-routing.module.ts
+import {  Routes } from '@angular/router';
+import { EmConstrucaoComponent } from './pages/em-construcao/em-construcao.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'em-construcao', component: EmConstrucaoComponent },
+  { path: '**', redirectTo: 'em-construcao' } // fallback opcional
+];
+
+
